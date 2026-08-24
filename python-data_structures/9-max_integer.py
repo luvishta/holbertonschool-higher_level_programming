838c1381616936467dc147e279592674cb1a72fe
luvishta <luvishta14@gmail.com>
2026-08-24 11:42:43 +0400

    task 10

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    big_num = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big_num:
+            big_num = my_list[i]
+    return (big_num)
