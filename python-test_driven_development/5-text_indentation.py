28a063cc23ef2d9a45054e24d208a70d63f4e4a1
luvishta <luvishta14@gmail.com>
2026-09-17 14:04:07 +0400

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"define the function"
+
+
+def text_indentation(text):
+    "Prints a text with 2 new lines after each of these characters: ., ? and :"
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    text = text.replace(".", ".\n\n")
+    text = text.replace(":", ":\n\n")
+    text = text.replace("?", "?\n\n")
+    print(text, end="")
