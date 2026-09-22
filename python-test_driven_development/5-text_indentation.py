e627d1e2403811bfdbdd38b5f5eeddac839e632f
luvishta <luvishta14@gmail.com>
2026-09-22 08:48:56 +0400

    task 7

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"define the function"
+"""Define a function that prints a text with 2 new lines after ., ? and :
+"""
 
 
 def text_indentation(text):
-    "Prints a text with 2 new lines after each of these characters: ., ? and :"
-
+    """This function prints text with 2 new lines after ., ? and :.
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace(".", ".\n\n")
-    text = text.replace(":", ":\n\n")
-    text = text.replace("?", "?\n\n")
-    print(text, end="")
+
+    line = ""
+    for char in text:
+        line += char
+        if char in [".", "?", ":"]:
+            print(line.strip())
+            print()
+            line = ""
+    if line.strip():
+        print(line.strip(), end="")
