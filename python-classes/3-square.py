f73e237ad6c3da6919de258e10508615b71f5f6e
luvishta <luvishta14@gmail.com>
2026-09-22 14:23:21 +0400

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+class Square:
+    """A square class"""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initialize class"""
+        if size is not int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size ** 2)
