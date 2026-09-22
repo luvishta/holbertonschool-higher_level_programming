35756fa0b700ae5b0e2c9d77cd567d844e32b879
luvishta <luvishta14@gmail.com>
2026-09-22 11:57:38 +0400

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """A square class"""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initialize class"""
+        if size is not int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
